0d9c6aca4553e1b4f294fa8258a5fbd7e994a8c8
lsh9295 <lsh929500@gmail.com>
2023-08-14 13:38:42 +0900

    test: jenkins build test

--- src/main/java/com/batton/memberservice/MemberServiceApplication.java
+++ src/main/java/com/batton/memberservice/MemberServiceApplication.java
@@ -14,5 +14,5 @@ public class MemberServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberServiceApplication.class, args);
     }
-
+    // jenkins build test
 }
